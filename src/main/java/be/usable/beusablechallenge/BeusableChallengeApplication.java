733d8306553e3ba02de47f068c4ed0bdc2082a2c
wcislo <jwcislo.it@gmail.com>
2024-06-25 11:41:44 +0200

    Initial commit

---
+++ src/main/java/be/usable/beusablechallenge/BeusableChallengeApplication.java
@@ -0,0 +1,13 @@
+package be.usable.beusablechallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeusableChallengeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeusableChallengeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/be/usable/beusablechallenge/BeusableChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package be.usable.beusablechallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeusableChallengeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
